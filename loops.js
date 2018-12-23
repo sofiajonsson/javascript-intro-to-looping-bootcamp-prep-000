7de23d51c462fae0dbf0d3d1727a10eee78a5cda
Sofia Jonsson <jonssonsofia93@gmail.com>
2018-12-23 03:52:37 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,7 +1,7 @@
 
   function forLoop(array){
   for (let i = 0; i < 25; i++){
-  return (array.push(`"I am ${i} strange loop${i === 0 ? '' : 's'}."`))
+  return (`"I am ${i} strange loop${i === 0 ? '' : 's'}."`))
   }
 }
   
