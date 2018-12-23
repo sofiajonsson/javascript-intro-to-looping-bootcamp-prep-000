c82e65c56a352ff8eea9a76042dbb772d058fd39
Sofia Jonsson <jonssonsofia93@gmail.com>
2018-12-23 03:45:49 +0000

    Automatically backed up by Learn

---
+++ loops.js
@@ -0,0 +1,28 @@
+
+  function forLoop(array){
+  for (let i = 0; i < 25; i++){
+    let i = 1;
+    array.push(i);
+  return (`"I am ${i} strange loop${i === 0 ? '' : 's'}."`)
+  }
+}
+  
+ function whileLoop(n){
+  let countdown = n
+  while (countdown > 0){
+    console.log(--countdown)
+  }
+  return 'done';
+}
+
+
+function doWhileLoop(array) {
+  let i = 0;
+  function incrementVariable() {
+    i = i + 1;
+  }
+  do {
+    array.pop();
+  } while (array.length > 0 && incrementVariable());
+  return array;
+}
