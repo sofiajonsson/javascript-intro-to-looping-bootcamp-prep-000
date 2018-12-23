80ddfeb74816d83e5949604c7ef1c50fcd034605
Sofia Jonsson <jonssonsofia93@gmail.com>
2018-12-23 04:05:25 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,22 +1,9 @@
-  describe('forLoop(array)', () => {
-    it('adds `"I am ${i} strange loop${i === 0 ? \'\' : \'s\'}."` to an array 25 times', () => {
-      const [array, t] = makeArray()
-      const strangeArray = forLoop(array)
-      const testArray = strangeArray.slice(array.length)
 
-      let first = "I am 1 strange loop."
-      let rest = "I am 24 strange loops."
-        
-      expect(strangeArray[11]).to.equal(first)
-      expect(strangeArray[34]).to.equal(rest)
-      expect(strangeArray.length).to.equal(t + 25)
-    })
-  })
  
  
   function forLoop(array){
   for (let i = 0; i < 25; i++){
-  array.slice(`"I am ${i} strange loop${i === 0 ? '' : 's'}."`)
+  array.push(`"I am ${i} strange loop${i === 0 ? '' : 's'}."`)
   }
 }
   
